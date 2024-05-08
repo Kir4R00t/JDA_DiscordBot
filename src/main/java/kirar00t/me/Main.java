342858788f571e3064232b5ef8f33e07d9ff198f
Kir4R00t <miki4sa@interia.pl>
2024-05-08 18:41:37 +0200

    initial commit

---
+++ src/main/java/kirar00t/me/Main.java
@@ -0,0 +1,31 @@
+package kirar00t.me;
+
+// JDA imports
+import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+
+// Misc imports
+import javax.security.auth.login.LoginException;
+import io.github.cdimascio.dotenv.Dotenv;
+
+public class Main extends ListenerAdapter {
+
+    public static void main(String[] args) throws LoginException {
+        // Load API token
+        Dotenv dotenv = Dotenv.load();
+        String token = dotenv.get("DISCORD");
+
+        JDABuilder.createDefault(token)
+                .addEventListeners(new Main())
+                .build();
+    }
+
+    @Override
+    public void onMessageReceived(MessageReceivedEvent event) {
+        String[] message = event.getMessage().getContentRaw().split(" ");
+        if (message.length > 0 && message[0].equalsIgnoreCase("!hello")) {
+            event.getChannel().sendMessage(STR."Hello, \{event.getAuthor().getAsMention()}!").queue();
+        }
+    }
+}
